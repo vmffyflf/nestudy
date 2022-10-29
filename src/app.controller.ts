3c4af658d1f20a0a75291db2bf0f044e97d5a227
j__h.106 <wlsghks245@gmail.com>
2022-10-29 14:51:48 +0900

    inital commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/goodbye')
+  getGoodbye(): string {
+    return this.appService.getGoodbye();
+  }
 }
